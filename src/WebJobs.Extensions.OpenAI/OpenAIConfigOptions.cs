6ee62f431b56ab868a35ca5fa82cebd94106c657
Aishwarya Bhandari <aibhandari@microsoft.com>
2024-02-01 11:03:46 -0800

    changing default values for OpenAIConfigOptions

--- src/WebJobs.Extensions.OpenAI/OpenAIConfigOptions.cs
+++ src/WebJobs.Extensions.OpenAI/OpenAIConfigOptions.cs
@@ -9,12 +9,12 @@ namespace WebJobs.Extensions.OpenAI;
 public class OpenAIConfigOptions
 {
     /// <summary>
-    /// Gets the storage connection name for storage account.
+    /// Gets the storage connection name for storage account. Default to "AzureWebJobsStorage".
     /// </summary>
-    public string StorageConnectionName { get; set; }
+    public string StorageConnectionName { get; set; } = "AzureWebJobsStorage";
 
     /// <summary>
-    /// Gets the storage collection name, which will be the name of the table.
+    /// Gets the storage collection name, which will be the name of the table. Default to "ChatBotRequests".
     /// </summary>
     public string? CollectionName { get; set; } = "ChatBotRequests";
 }

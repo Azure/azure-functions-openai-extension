88d262059e6764db46e07ab62a638a411da7a845
Shreyas Gopalakrishna <shreyasg@microsoft.com>
2024-02-11 18:39:20 -0600

    Java samples initial commit

---
+++ samples/chat/java/src/main/java/com/azfs/ChatBot.java
@@ -0,0 +1,78 @@
+package com.azfs;
+
+import org.json.JSONObject;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.OutputBinding;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.BindingName;
+import com.microsoft.azure.functions.annotation.CustomBinding;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import java.util.Optional;
+
+/**
+ * Azure Functions ChatBot.
+ */
+public class ChatBot {
+
+    @FunctionName("CreateChatBot")
+    public HttpResponseMessage createChatBot(
+        @HttpTrigger(
+            name = "req", 
+            methods = {HttpMethod.PUT},
+            authLevel = AuthorizationLevel.ANONYMOUS, 
+            route = "chats/{chatId}") 
+            HttpRequestMessage<Optional<CreateRequest>> request,
+        @BindingName("chatId") String chatId,
+        @CustomBinding(direction = "out", name = "ChatBotCreate", type = "chatBotCreate") OutputBinding<ChatBotCreateRequest> message,
+        final ExecutionContext context) {
+               
+            ChatBotCreateRequest chatBotCreateRequest = new ChatBotCreateRequest(chatId, request.getBody().get().getInstructions());
+            message.setValue(chatBotCreateRequest);
+
+            JSONObject response = new JSONObject();
+            response.put("chatId", chatId);
+
+            return request.createResponseBuilder(HttpStatus.ACCEPTED)
+                .header("Content-Type", "application/json")
+                .body(response.toString())
+                .build();
+    }
+
+    public class CreateRequest {
+        public String instructions;
+
+        public String getInstructions() {
+            return instructions;
+        }
+
+        public void setInstructions(String instructions) {
+            this.instructions = instructions;
+        }
+    }
+
+    public class ChatBotCreateRequest {
+
+        public String id;
+        public String instructions;
+
+        public ChatBotCreateRequest(String id, String instructions) {
+            this.id = id;
+            this.instructions = instructions;
+        }
+
+        public String getChatId() {
+            return id;
+        }
+
+        public void setChatId(String id) {
+            this.id = id;
+        }
+    }
+
+}

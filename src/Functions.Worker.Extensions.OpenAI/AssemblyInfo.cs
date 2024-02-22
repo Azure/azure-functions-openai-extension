9e278b87601e3c3f6414652fd299e2b24bc12c7e
Aishwarya Bhandari <aibhandari@microsoft.com>
2024-02-22 14:41:41 -0800

    trying again

--- src/Functions.Worker.Extensions.OpenAI/AssemblyInfo.cs
+++ src/Functions.Worker.Extensions.OpenAI/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 
 using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 
-[assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.OpenAI", "0.10.0-alpha")]
+[assembly: ExtensionInformation("Microsoft.Azure.WebJobs.Extensions.OpenAI", "0.11.0-alpha")]

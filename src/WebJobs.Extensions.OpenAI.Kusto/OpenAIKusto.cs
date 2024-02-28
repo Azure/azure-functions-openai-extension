f0ff8cf3db35dceb4b528b2aa93b791ebf0e51a4
Aishwarya Bhandari <aibhandari@microsoft.com>
2024-02-28 15:15:48 -0800

    reverting some changes back

---
+++ src/WebJobs.Extensions.OpenAI.Kusto/OpenAIKusto.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Extensions.OpenAI.Kusto;
+using Microsoft.Azure.WebJobs.Extensions.OpenAI.Search;
+using Microsoft.Extensions.DependencyInjection;
+
+// Reference: https://docs.microsoft.com/en-us/azure/azure-functions/functions-dotnet-dependency-injection
+[assembly: FunctionsStartup(typeof(OpenAIKusto))]
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenAI.Kusto;
+
+class OpenAIKusto : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddSingleton<ISearchProvider, KustoSearchProvider>();
+    }
+}

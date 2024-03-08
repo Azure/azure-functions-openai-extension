880abf44e7eb6f39c2c4df0a9b707528a3768c0c
Aishwarya Bhandari <aibhandari@microsoft.com>
2024-03-08 12:14:10 -0800

    changing the project since it was giving a weird error before

--- samples/embeddings/csharp-ooproc/Embedding/Program.cs
+++ samples/embeddings/csharp-ooproc/Embedding/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services =>

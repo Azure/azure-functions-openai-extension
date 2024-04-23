950d659729ce6197286d41db20e394d69629153e
Aishwarya Bhandari <37918412+aishwaryabh@users.noreply.github.com>
2024-04-23 09:33:23 -0700

    Cosmos DB Search Provider support (#59)
    * changes
    
    * adding build props change
    
    * fixing build
    
    * comments
    
    * removing ooproc config opiton
    
    * addressing comments
    
    * addressing comments

---
+++ src/WebJobs.Extensions.OpenAI.CosmosDBSearch/CosmosDBSearchConfigOptions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenAI.CosmosDBSearch;
+
+/// <summary>
+/// Open AI Configuration Options used for reading host.json values.
+/// </summary>
+public class CosmosDBSearchConfigOptions
+{
+    // Number of dimensions for vector similarity. The maximum number of supported dimensions is 2000.
+    public int VectorSearchDimensions { get; set; } = 1536;
+
+    // This integer is the number of clusters that the inverted file (IVF) index uses to group the vector data
+    public int NumLists { get; set; } = 1;
+}

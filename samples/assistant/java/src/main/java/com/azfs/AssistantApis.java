db423b3fffad9a77c35f5b932d2e97c0cf0fc239
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2024-05-17 11:04:14 -0500

    Update AssistantApis.java
    Added new samples and Manvir's changes

--- samples/assistant/java/src/main/java/com/azfs/AssistantApis.java
+++ samples/assistant/java/src/main/java/com/azfs/AssistantApis.java
@@ -1,104 +1,160 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
-package com.azfs;
+---
+title: Azure OpenAI Assistant Post Input Binding for Azure Functions
+description: Learn how to use the Azure OpenAI assistant post input binding to query chat bots during function execution in Azure Functions.
+ms.topic: reference
+ms.date: 05/07/2024
+zone_pivot_groups: programming-languages-set-functions
+---
 
-import java.util.List;
-import java.util.Optional;
+# Azure OpenAI Assistant Post Input Binding for Azure Functions
 
-import com.microsoft.azure.functions.openai.annotation.assistant.*;
-import org.json.JSONObject;
+[!INCLUDE [preview-support](../../includes/functions-openai-support-limitations.md)]
 
-import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.HttpMethod;
-import com.microsoft.azure.functions.HttpRequestMessage;
-import com.microsoft.azure.functions.HttpResponseMessage;
-import com.microsoft.azure.functions.HttpStatus;
-import com.microsoft.azure.functions.OutputBinding;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
-import com.microsoft.azure.functions.annotation.BindingName;
-import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.HttpTrigger;
+The Azure OpenAI assistant post input binding lets you send prompts to assistant chat bots.
 
-/**
- * Defines HTTP APIs for interacting with assistants.
- */
-public class AssistantApis {
+For information on setup and configuration details of the Azure OpenAI extension, see [Azure OpenAI extensions for Azure Functions](./functions-bindings-openai.md). To learn more about Azure OpenAI assistants, see [Azure OpenAI Assistants API](../ai-services/openai/concepts/assistants.md).
+::: zone pivot="programming-language-javascript,programming-language-typescript"  
+> [!NOTE]  
+> References and examples are only provided for the [Node.js v4 model](./functions-reference-node.md?pivots=nodejs-model-v4).
+::: zone-end  
+::: zone pivot="programming-language-python"  
+> [!NOTE]  
+> References and examples are only provided for the [Python v2 model](functions-reference-python.md?pivots=python-mode-decorators#development-options).
+::: zone-end  
 
-    /*
-     * HTTP PUT function that creates a new assistant chat bot with the specified ID.
-     */
-    @FunctionName("CreateAssistant")
-    public HttpResponseMessage createAssistant(
-        @HttpTrigger(
-            name = "req", 
-            methods = {HttpMethod.PUT}, 
-            authLevel = AuthorizationLevel.ANONYMOUS, 
-            route = "assistants/{assistantId}") 
-            HttpRequestMessage<Optional<String>> request,
-        @BindingName("assistantId") String assistantId,
-        @AssistantCreate(name = "AssistantCreate") OutputBinding<AssistantCreateRequest> message,
-        final ExecutionContext context) {
-            context.getLogger().info("Java HTTP trigger processed a request.");
-            
-            String instructions = "Don't make assumptions about what values to plug into functions.\n" +
-                    "Ask for clarification if a user request is ambiguous.";
+## Example
 
-            AssistantCreateRequest assistantCreateRequest = new AssistantCreateRequest(assistantId, instructions);
-            message.setValue(assistantCreateRequest);
-            JSONObject response = new JSONObject();
-            response.put("assistantId", assistantId);
-            
-            return request.createResponseBuilder(HttpStatus.CREATED)
-                .header("Content-Type", "application/json")
-                .body(response.toString())
-                .build();    
-    }
+::: zone pivot="programming-language-csharp"  
+A C# function can be created using one of the following C# modes:
 
-    /*
-     * HTTP GET function that queries the conversation history of the assistant chat bot.
-     */   
-    @FunctionName("GetChatState")
-    public HttpResponseMessage getChatState(
-        @HttpTrigger(
-            name = "req",
-            methods = {HttpMethod.GET}, 
-            authLevel = AuthorizationLevel.ANONYMOUS,
-            route = "assistants/{assistantId}") 
-            HttpRequestMessage<Optional<String>> request,
-        @BindingName("assistantId") String assistantId,        
-        @AssistantQuery(name = "AssistantState", id = "{assistantId}", timestampUtc = "{Query.timestampUTC}") AssistantState state,
-        final ExecutionContext context) {
-            return request.createResponseBuilder(HttpStatus.OK)
-                .header("Content-Type", "application/json")
-                .body(state)
-                .build();
-    }
+[!INCLUDE [dotnet-execution](../../includes/functions-dotnet-execution-model.md)]
 
-    /*
-     * HTTP POST function that sends user prompts to the assistant chat bot.
-     */ 
-    @FunctionName("PostUserResponse")
-    public HttpResponseMessage postUserResponse(
-        @HttpTrigger(
-            name = "req",
-            methods = {HttpMethod.POST}, 
-            authLevel = AuthorizationLevel.ANONYMOUS,
-            route = "assistants/{assistantId}") 
-            HttpRequestMessage<Optional<String>> request,
-        @BindingName("assistantId") String assistantId,        
-        @AssistantPost(name="newMessages", id = "{assistantId}", model = "%CHAT_MODEL_DEPLOYMENT_NAME%", userMessage = "{Query.message}") AssistantState state,
-        final ExecutionContext context) {
-            
-            List<ChatMessage> recentMessages = state.getRecentMessages();
-            String response = recentMessages.isEmpty() ? "No response returned." : recentMessages.get(recentMessages.size() - 1).getContent();
-            
-            return request.createResponseBuilder(HttpStatus.OK)
-                .header("Content-Type", "application/json")
-                .body(response)
-                .build();
-    }
-}
+### [Isolated process](#tab/isolated-process)
 
+This example demonstrates the creation process, where the HTTP POST function that sends user prompts to the assistant chat bot. The response to the prompt is returned in the HTTP response.
+
+:::code language="csharp" source="~/functions-openai-extension/samples/assistant/csharp-ooproc/AssistantApis.cs" range="58-78"::: 
+<!-- 
+### [In-process](#tab/in-process)
+
+[!INCLUDE [functions-examples-not-available-note](../../includes/functions-examples-not-available-note.md)] -->
+
+---
+
+::: zone-end  
+::: zone pivot="programming-language-java"
+
+This example demonstrates the creation process, where the HTTP POST function that sends user prompts to the assistant chat bot. The response to the prompt is returned in the HTTP response.
+
+:::code language="java" source="~/functions-openai-extension/samples/assistant/java/src/main/java/com/azfs/AssistantApis.java" range="83-103":::
+
+::: zone-end  
+::: zone pivot="programming-language-javascript"
+[!INCLUDE [functions-examples-not-available-note](../../includes/functions-examples-not-available-note.md)]
+<!---uncomment when code example is available:
+{{This comes from the example code comment}} 
+
+:::code language="javascript" source="~/functions-openai-extension/samples/{{link to the correct sample.js}}" range="{{named is better than range}}":::
+
+{{Add more examples if available}}
+-->
+::: zone-end  
+::: zone pivot="programming-language-typescript"
+
+This example demonstrates the creation process, where the HTTP POST function that sends user prompts to the assistant chat bot. The response to the prompt is returned in the HTTP response.
+
+:::code language="typescript" source="~/functions-openai-extension/samples/assistant/nodejs/src/functions/assistantApis.ts" range="32-50":::
+
+ ::: zone-end  
+::: zone pivot="programming-language-powershell"  
+
+This example demonstrates the creation process, where the HTTP POST function that sends user prompts to the assistant chat bot. The response to the prompt is returned in the HTTP response.
+
+Here's the _function.json_ file for post user query:
+
+:::code language="json" source="~/functions-openai-extension/samples/assistant/powershell/PostUserQuery/function.json" :::
+
+For more information about *function.json* file properties, see the [Configuration](#configuration) section.
+
+:::code language="powershell" source="~/functions-openai-extension/samples/assistant/powershell/PostUserQuery/run.ps1" :::
+
+::: zone-end   
+::: zone pivot="programming-language-python"  
+This example demonstrates the creation process, where the HTTP POST function that sends user prompts to the assistant chat bot. The response to the prompt is returned in the HTTP response.
+
+:::code language="python" source="~/functions-openai-extension/samples/assistant/python/assistant_apis.py" range="25-34":::
+
+::: zone-end  
+<!--- End code examples section -->  
+::: zone pivot="programming-language-csharp"  
+## Attributes
+
+Apply the `PostUserQuery` attribute to define an assistant post input binding, which supports these parameters:
+
+| Parameter | Description |
+| --------- | ----------- |
+| **Id** | The ID of the assistant to update. |
+| **Model** | The name of the OpenAI chat model to use. For Azure OpenAI, this value is the name of the model deployment. |
+
+::: zone-end
+::: zone pivot="programming-language-java"
+## Annotations
+
+The `PostUserQuery` annotation enables you to define an assistant post input binding, which supports these parameters: 
+
+| Element | Description |
+| ------- | ----------- |
+| **name** | The name of the output binding. |
+| **id** | The ID of the assistant to update. |
+| **model** | The name of the OpenAI chat model to use. For Azure OpenAI, this value is the name of the model deployment. |
+
+::: zone-end  
+::: zone pivot="programming-language-python"  
+## Decorators
+<!--- Replace with typed decorator when available.-->
+During the preview, define the output binding as a `generic_output_binding` binding of type `postUserQuery`, which supports these parameters:
+
+|Parameter | Description |
+|---------|-------------|
+| **arg_name** | The name of the variable that represents the binding parameter. |
+| **id** | The ID of the assistant to update. |
+| **model** | The name of the OpenAI chat model to use. For Azure OpenAI, this value is the name of the model deployment. |
+
+::: zone-end
+::: zone pivot="programming-language-powershell"  
+## Configuration  
+
+The binding supports these configuration properties that you set in the function.json file.
+
+|Property | Description |
+|-----------------------|-------------|
+| **type** | Must be `PostUserQuery`. |
+| **direction** | Must be `out`. |
+| **name** | The name of the output binding. |
+| **id** | The ID of the assistant to update. |
+| **model** | The name of the OpenAI chat model to use. For Azure OpenAI, this value is the name of the model deployment. |
+ 
+::: zone-end  
+::: zone pivot="programming-language-javascript,programming-language-typescript"  
+## Configuration
+
+The binding supports these properties, which are defined in your code: 
+
+|Property | Description |
+|-----------------------|-------------|
+| **id** | The ID of the assistant to update. |
+| **model** | The name of the OpenAI chat model to use. For Azure OpenAI, this value is the name of the model deployment. |
+
+::: zone-end  
+
+## Usage
+
+See the [Example section](#example) for complete examples.
+
+## Related content
+
++ [Assistant samples](https://github.com/Azure/azure-functions-openai-extension/tree/main/samples/assistant)
++ [Azure OpenAI extension](functions-bindings-openai.md)
++ [Azure OpenAI assistant trigger](functions-bindings-openai-assistant-trigger.md)
++ [Azure OpenAI assistant query input binding](functions-bindings-openai-assistantcreate-output.md)
++ [Azure OpenAI assistant create output binding](functions-bindings-openai-assistantcreate-output.md)

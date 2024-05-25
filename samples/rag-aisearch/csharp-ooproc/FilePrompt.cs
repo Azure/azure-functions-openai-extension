702615dbf5c6967ff6f14a078164277511bb8f61
Paul Yuknewicz <paulyuk@microsoft.com>
2024-05-25 11:09:24 -0700

    Converting to modern ASP.NET with styles
    Signed-off-by: Paul Yuknewicz <paulyuk@microsoft.com>

--- samples/rag-aisearch/csharp-ooproc/FilePrompt.cs
+++ samples/rag-aisearch/csharp-ooproc/FilePrompt.cs
@@ -1,19 +1,97 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Net;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.OpenAI.Embeddings;
 using Microsoft.Azure.Functions.Worker.Extensions.OpenAI.Search;
-using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
 
 namespace SemanticAISearchEmbeddings;
 
-public static class FilePrompt
+public class FilePrompt
 {
+    readonly ILogger _logger;
+
+    public FilePrompt(ILoggerFactory loggerFactory)
+    {
+        this._logger = loggerFactory.CreateLogger<FilePrompt>();
+    }
+
+    [Function("IngestFile")]
+    public async Task<EmbeddingsStoreOutputResponse> IngestFile(
+        [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req,
+        [FromBody] EmbeddingsRequest body 
+    )
+    {
+
+        using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8))
+        {
+            var jsonBody = await reader.ReadToEndAsync();
+            this._logger.LogInformation(jsonBody); // Log the JSON body
+            // deserialize the JSON body into the EmbeddingsRequest object
+            try
+            {
+                body = JsonSerializer.Deserialize<EmbeddingsRequest>(jsonBody);
+            }
+            catch (JsonException ex)
+            {
+                this._logger.LogInformation($"Message: {ex.Message}");
+                this._logger.LogInformation($"StackTrace: {ex.StackTrace}");
+            }
+        }
+        if (body == null || body.Url == null)
+        {
+            throw new ArgumentException(
+                "Invalid request body. Make sure that you pass in {\"Url\": value } as the request body."
+            );
+        }
+
+        Uri uri = new(body.Url);
+        string filename = Path.GetFileName(uri.AbsolutePath);
+
+        return new EmbeddingsStoreOutputResponse
+        {
+            HttpResponse = new OkObjectResult("Ingested file"),
+            SearchableDocument = new SearchableDocument(filename)
+        };
+    }
+
+    [Function("PromptFile")]
+    public static IActionResult PromptFile(
+        [HttpTrigger(AuthorizationLevel.Function, "post")] SemanticSearchRequest unused,
+        [SemanticSearchInput(
+            "AISearchEndpoint",
+            "openai-index",
+            Query = "{Prompt}",
+            ChatModel = "%CHAT_MODEL_DEPLOYMENT_NAME%",
+            EmbeddingsModel = "%EMBEDDING_MODEL_DEPLOYMENT_NAME%"
+        )]
+            SemanticSearchContext result
+    )
+    {
+        return new ContentResult { Content = result.Response, ContentType = "text/plain" };
+    }
+
+    public class EmbeddingsStoreOutputResponse
+    {
+        [EmbeddingsStoreOutput(
+            "{Url}",
+            InputType.Url,
+            "AISearchEndpoint",
+            "openai-index",
+            Model = "%EMBEDDING_MODEL_DEPLOYMENT_NAME%"
+        )]
+        public required SearchableDocument SearchableDocument { get; init; }
+
+        [HttpResult]
+        public IActionResult? HttpResponse { get; set; }
+    }
+
     public class EmbeddingsRequest
     {
         [JsonPropertyName("Url")]
@@ -24,47 +102,5 @@ public static class FilePrompt
     {
         [JsonPropertyName("Prompt")]
         public string? Prompt { get; set; }
-    }
-
-    [Function("IngestFile")]
-    public static async Task<EmbeddingsStoreOutputResponse> IngestFile(
-        [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
-    {
-        using StreamReader reader = new(req.Body);
-        string request = await reader.ReadToEndAsync();
-
-        EmbeddingsRequest? requestBody = JsonSerializer.Deserialize<EmbeddingsRequest>(request);
-
-        if (requestBody == null || requestBody.Url == null)
-        {
-            throw new ArgumentException("Invalid request body. Make sure that you pass in {\"Url\": value } as the request body.");
-        }
-
-        Uri uri = new(requestBody.Url);
-        string filename = Path.GetFileName(uri.AbsolutePath);
-
-        HttpResponseData response = req.CreateResponse(HttpStatusCode.Created);
-
-        return new EmbeddingsStoreOutputResponse
-        {
-            HttpResponse = response,
-            SearchableDocument = new SearchableDocument(filename)
-        };
-    }
-
-    public class EmbeddingsStoreOutputResponse
-    {
-        [EmbeddingsStoreOutput("{Url}", InputType.Url, "AISearchEndpoint", "openai-index", Model = "%EMBEDDING_MODEL_DEPLOYMENT_NAME%")]
-        public required SearchableDocument SearchableDocument { get; init; }
-
-        public HttpResponseData? HttpResponse { get; set; }
-    }
-
-    [Function("PromptFile")]
-    public static IActionResult PromptFile(
-        [HttpTrigger(AuthorizationLevel.Function, "post")] SemanticSearchRequest unused,
-        [SemanticSearchInput("AISearchEndpoint", "openai-index", Query = "{Prompt}", ChatModel = "%CHAT_MODEL_DEPLOYMENT_NAME%", EmbeddingsModel = "%EMBEDDING_MODEL_DEPLOYMENT_NAME%")] SemanticSearchContext result)
-    {
-        return new ContentResult { Content = result.Response, ContentType = "text/plain" };
     }
 }
